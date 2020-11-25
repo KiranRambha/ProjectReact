5d7e5aa9e8220147884c83ab0f0e7c15274f60a5
kiran <kiran.rambha.1995@gmail.com>
2020-11-25 13:30:01 +0000

    test

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import IsVisible from 'react-is-visible';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserGraduate, faMapPin } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,76 +11,97 @@ export default function Education({ darkMode }) {
           <FontAwesomeIcon icon={faUserGraduate} />
           <u className='pl-2'>Education</u>
         </div>
-        <div className='pb-10 flex'>
-          <div
-            className={`w-1 ${darkMode}:bg-gray-200 bg-gray-500 rounded-full md:ml-6 opacity-25`}
-          ></div>
-          <div className='-ml-2'>
-            <div className='py-4 flex'>
+        <IsVisible once>
+          {(isVisible) => (
+            <div className='pb-10 flex'>
               <div
-                className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
+                className={`w-1 ${darkMode}:bg-gray-200 bg-gray-500 rounded-full md:ml-6 opacity-25`}
               ></div>
-              <div className='ml-8'>
-                <div className='mt-1.5 flex items-baseline'>
-                  <h6 class='font-semibold'>
-                    <span>Royal Holloway</span>
-                    <span className='hidden lg:contents'>
-                      , University of London
-                    </span>
-                  </h6>
-                  <h6 class='text-xs ml-2'>(2013 - 2017)</h6>
+              <div className='-ml-2'>
+                <div
+                  className={`py-4 flex ${
+                    isVisible ? 'animated-3 fadeInDown' : 'hidden'
+                  }`}
+                >
+                  <div
+                    className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
+                  ></div>
+                  <div className='ml-8'>
+                    <div className='mt-1.5 flex items-baseline'>
+                      <h6 class='font-semibold'>
+                        <span>Royal Holloway</span>
+                        <span className='hidden lg:contents'>
+                          , University of London
+                        </span>
+                      </h6>
+                      <h6 class='text-xs ml-2'>(2013 - 2017)</h6>
+                    </div>
+                    <h6 class='text-sm'>
+                      BSc Computer Science (Year in Industry)
+                    </h6>
+                    <div class='mt-2 flex items-center'>
+                      <FontAwesomeIcon icon={faMapPin} />
+                      <h6 class='font-semibold text-xs ml-2'>London, UK</h6>
+                    </div>
+                  </div>
                 </div>
-                <h6 class='text-sm'>BSc Computer Science (Year in Industry)</h6>
-                <div class='mt-2 flex items-center'>
-                  <FontAwesomeIcon icon={faMapPin} />
-                  <h6 class='font-semibold text-xs ml-2'>London, UK</h6>
+                <div
+                  className={`py-4 flex ${
+                    isVisible ? 'animated-6 fadeInDown' : 'hidden'
+                  }`}
+                >
+                  <div
+                    className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
+                  ></div>
+                  <div className='ml-8'>
+                    <div className='mt-1.5 flex items-baseline'>
+                      <h6 class='font-semibold'>
+                        Sri Chaitanya{' '}
+                        <span className='hidden lg:contents'>
+                          Junior College
+                        </span>
+                      </h6>
+                      <h6 class='text-xs ml-2'>(2011 - 2013)</h6>
+                    </div>
+                    <h6 class='text-sm'>Intermediate Education (A-Level)</h6>
+                    <div class='mt-2 flex items-center'>
+                      <FontAwesomeIcon icon={faMapPin} />
+                      <h6 class='font-semibold text-xs ml-2'>
+                        Vijayawada, AP, India
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className={`py-4 flex ${
+                    isVisible ? 'animated-9 fadeInDown' : 'hidden'
+                  }`}
+                >
+                  <div
+                    className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
+                  ></div>
+                  <div className='ml-8'>
+                    <div className='mt-1.5 flex items-baseline'>
+                      <h6 class='font-semibold'>
+                        Gowtham{' '}
+                        <span className='hidden lg:contents'>Concept</span>{' '}
+                        School
+                      </h6>
+                      <h6 class='text-xs ml-2'>(2007-2011)</h6>
+                    </div>
+                    <h6 class='text-sm'>High School</h6>
+                    <div class='mt-2 flex items-center'>
+                      <FontAwesomeIcon icon={faMapPin} />
+                      <h6 class='font-semibold text-xs ml-2'>
+                        Gudivada, AP, India
+                      </h6>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className='py-4 flex'>
-              <div
-                className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
-              ></div>
-              <div className='ml-8'>
-                <div className='mt-1.5 flex items-baseline'>
-                  <h6 class='font-semibold'>
-                    Sri Chaitanya{' '}
-                    <span className='hidden lg:contents'>Junior College</span>
-                  </h6>
-                  <h6 class='text-xs ml-2'>(2011 - 2013)</h6>
-                </div>
-                <h6 class='text-sm'>Intermediate Education (A-Level)</h6>
-                <div class='mt-2 flex items-center'>
-                  <FontAwesomeIcon icon={faMapPin} />
-                  <h6 class='font-semibold text-xs ml-2'>
-                    Vijayawada, AP, India
-                  </h6>
-                </div>
-              </div>
-            </div>
-            <div className='py-4 flex'>
-              <div
-                className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
-              ></div>
-              <div className='ml-8'>
-                <div className='mt-1.5 flex items-baseline'>
-                  <h6 class='font-semibold'>
-                    Gowtham <span className='hidden lg:contents'>Concept</span>{' '}
-                    School
-                  </h6>
-                  <h6 class='text-xs ml-2'>(2007-2011)</h6>
-                </div>
-                <h6 class='text-sm'>High School</h6>
-                <div class='mt-2 flex items-center'>
-                  <FontAwesomeIcon icon={faMapPin} />
-                  <h6 class='font-semibold text-xs ml-2'>
-                    Gudivada, AP, India
-                  </h6>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+          )}
+        </IsVisible>
       </div>
     </section>
   );
--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import IsVisible from 'react-is-visible';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLaptopCode, faMapPin } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,95 +11,115 @@ export default function Experience({ darkMode }) {
           <FontAwesomeIcon icon={faLaptopCode} />
           <u className='pl-2'>Experience</u>
         </div>
-        <div className='pb-10 flex'>
-          <div
-            className={`w-1 ${darkMode}:bg-gray-200 bg-gray-500 rounded-full md:ml-6 opacity-25`}
-          ></div>
-          <div className='-ml-2'>
-            <div className='py-4 flex'>
+        <IsVisible once>
+          {(isVisible) => (
+            <div className='pb-10 flex'>
               <div
-                className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
+                className={`w-1 ${darkMode}:bg-gray-200 bg-gray-500 rounded-full md:ml-6 opacity-25`}
               ></div>
-              <div className='ml-8'>
-                <div className='mt-1.5 flex items-baseline'>
-                  <h6 class='font-semibold'>Accenture UK</h6>
-                  <h6 class='text-xs ml-2'>(Dec '19 - Present)</h6>
+              <div className='-ml-2'>
+                <div
+                  className={`py-4 flex ${
+                    isVisible ? 'animated-3 fadeInDown' : 'hidden'
+                  }`}
+                >
+                  <div
+                    className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
+                  ></div>
+                  <div className='ml-8'>
+                    <div className='mt-1.5 flex items-baseline'>
+                      <h6 class='font-semibold'>Accenture UK</h6>
+                      <h6 class='text-xs ml-2'>(Dec '19 - Present)</h6>
+                    </div>
+                    <h6 class='text-sm'>Senior Software Engineer Analyst</h6>
+                    <div class='mt-2 flex items-center'>
+                      <FontAwesomeIcon icon={faMapPin} />
+                      <h6 class='font-semibold text-xs ml-2'>London, UK</h6>
+                    </div>
+                    <h6 class='text-xs mt-2'>
+                      <strong>Specialized in:</strong> Nodejs, Reactjs, Alexa
+                      Skill Kit, AWS
+                    </h6>
+                  </div>
                 </div>
-                <h6 class='text-sm'>Senior Software Engineer Analyst</h6>
-                <div class='mt-2 flex items-center'>
-                  <FontAwesomeIcon icon={faMapPin} />
-                  <h6 class='font-semibold text-xs ml-2'>London, UK</h6>
+                <div
+                  className={`py-4 flex ${
+                    isVisible ? 'animated-6 fadeInDown' : 'hidden'
+                  }`}
+                >
+                  <div
+                    className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
+                  ></div>
+                  <div className='ml-8'>
+                    <div className='mt-1.5 flex items-baseline'>
+                      <h6 class='font-semibold'>Accenture UK</h6>
+                      <h6 class='text-xs ml-2'>(Dec '18 - Nov '19')</h6>
+                    </div>
+                    <h6 class='text-sm'>Software Engineer Analyst</h6>
+                    <div class='mt-2 flex items-center'>
+                      <FontAwesomeIcon icon={faMapPin} />
+                      <h6 class='font-semibold text-xs ml-2'>London, UK</h6>
+                    </div>
+                    <h6 class='text-xs mt-2'>
+                      <strong>Specialized in:</strong> Microservice
+                      Architecture, Chatbots
+                    </h6>
+                  </div>
                 </div>
-                <h6 class='text-xs mt-2'>
-                  <strong>Specialized in:</strong> Nodejs, Reactjs, Alexa Skill
-                  Kit, AWS
-                </h6>
+                <div
+                  className={`py-4 flex ${
+                    isVisible ? 'animated-9 fadeInDown' : 'hidden'
+                  }`}
+                >
+                  <div
+                    className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
+                  ></div>
+                  <div className='ml-8'>
+                    <div className='mt-1.5 flex items-baseline'>
+                      <h6 class='font-semibold'>Accenture UK</h6>
+                      <h6 class='text-xs ml-2'>(Oct '17 - Nov '18')</h6>
+                    </div>
+                    <h6 class='text-sm'>Software Engineer Associate</h6>
+                    <div class='mt-2 flex items-center'>
+                      <FontAwesomeIcon icon={faMapPin} />
+                      <h6 class='font-semibold text-xs ml-2'>London, UK</h6>
+                    </div>
+                    <h6 class='text-xs mt-2'>
+                      <strong>Specialized in:</strong> Murex, Python, HTML,
+                      Javascript, CSS
+                    </h6>
+                  </div>
+                </div>
+                <div
+                  className={`py-4 flex ${
+                    isVisible ? 'animated-12 fadeInDown' : 'hidden'
+                  }`}
+                >
+                  <div
+                    className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
+                  ></div>
+                  <div className='ml-8'>
+                    <div className='mt-1.5 flex items-baseline'>
+                      <h6 class='font-semibold'>Accenture UK</h6>
+                      <h6 class='text-xs ml-2'>(Jun '15 - Sep '16')</h6>
+                    </div>
+                    <h6 class='text-sm'>
+                      Software Engineer (Industrial Placement)
+                    </h6>
+                    <div class='mt-2 flex items-center'>
+                      <FontAwesomeIcon icon={faMapPin} />
+                      <h6 class='font-semibold text-xs ml-2'>London, UK</h6>
+                    </div>
+                    <h6 class='text-xs mt-2'>
+                      <strong>Specialized in:</strong> C#, ASP.NET, HTML, CSS,
+                      jQuery, AJAX
+                    </h6>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className='py-4 flex'>
-              <div
-                className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
-              ></div>
-              <div className='ml-8'>
-                <div className='mt-1.5 flex items-baseline'>
-                  <h6 class='font-semibold'>Accenture UK</h6>
-                  <h6 class='text-xs ml-2'>(Dec '18 - Nov '19')</h6>
-                </div>
-                <h6 class='text-sm'>Software Engineer Analyst</h6>
-                <div class='mt-2 flex items-center'>
-                  <FontAwesomeIcon icon={faMapPin} />
-                  <h6 class='font-semibold text-xs ml-2'>London, UK</h6>
-                </div>
-                <h6 class='text-xs mt-2'>
-                  <strong>Specialized in:</strong> Microservice Architecture,
-                  Chatbots
-                </h6>
-              </div>
-            </div>
-            <div className='py-4 flex'>
-              <div
-                className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
-              ></div>
-              <div className='ml-8'>
-                <div className='mt-1.5 flex items-baseline'>
-                  <h6 class='font-semibold'>Accenture UK</h6>
-                  <h6 class='text-xs ml-2'>(Oct '17 - Nov '18')</h6>
-                </div>
-                <h6 class='text-sm'>Software Engineer Associate</h6>
-                <div class='mt-2 flex items-center'>
-                  <FontAwesomeIcon icon={faMapPin} />
-                  <h6 class='font-semibold text-xs ml-2'>London, UK</h6>
-                </div>
-                <h6 class='text-xs mt-2'>
-                  <strong>Specialized in:</strong> Murex, Python, HTML,
-                  Javascript, CSS
-                </h6>
-              </div>
-            </div>
-            <div className='py-4 flex'>
-              <div
-                className={`relative mt-3 w-3 h-3 rounded-full shadow-lg z-2 bg-black ${darkMode}:bg-white`}
-              ></div>
-              <div className='ml-8'>
-                <div className='mt-1.5 flex items-baseline'>
-                  <h6 class='font-semibold'>Accenture UK</h6>
-                  <h6 class='text-xs ml-2'>(Jun '15 - Sep '16')</h6>
-                </div>
-                <h6 class='text-sm'>
-                  Software Engineer (Industrial Placement)
-                </h6>
-                <div class='mt-2 flex items-center'>
-                  <FontAwesomeIcon icon={faMapPin} />
-                  <h6 class='font-semibold text-xs ml-2'>London, UK</h6>
-                </div>
-                <h6 class='text-xs mt-2'>
-                  <strong>Specialized in:</strong> C#, ASP.NET, HTML, CSS,
-                  jQuery, AJAX
-                </h6>
-              </div>
-            </div>
-          </div>
-        </div>
+          )}
+        </IsVisible>
       </div>
     </section>
   );
