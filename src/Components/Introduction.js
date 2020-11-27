bab4e9bcf80e0c7f011ff9764b1e3b2e4e27da03
kiran <kiran.rambha.1995@gmail.com>
2020-11-27 10:31:59 +0000

    grammar changes

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -9,7 +9,7 @@ export default function Introduction(props) {
           className={`${props.darkMode}:bg-black ${props.darkMode}:text-white font-header pt-0 lg:pt-10 pb-10 md:pt-10 text-4xl md:w-3/5 lg:w-3/5 md:float-left lg:float-left`}
         >
           Full-Stack Developer based in London.
-          <br /> Currently working as Sr. Software Engineer at Accenture UK.
+          <br /> Currently working as a Sr. Software Engineer at Accenture UK.
         </div>
         <div className='md:w-2/5 lg:w-2/5 pl-16 lg:pl-28 md:float-left lg:float-left md:py-12 lg:pt-0'>
           <img
