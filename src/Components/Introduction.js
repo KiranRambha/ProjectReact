85ff11a04419e3138792159116ac1f133cd8a18f
kiran <kiran.rambha.1995@gmail.com>
2021-01-06 07:06:09 +0000

    margins fixed

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -9,7 +9,7 @@ export default function Introduction(props) {
         className={`${props.darkMode}:bg-black pb-8 md:pb-0 lg:pb-0 flex flex-col-reverse md:flex-row`}
       >
         <div
-          className={`${props.darkMode}:bg-black ${props.darkMode}:text-white font-header pt-0 md:pb-10 md:pt-20 lg:pt-12 xl:pt-16 text-3xl md:text-4xl md:w-3/5 lg:w-3/5 md:float-left lg:float-left text-center md:text-left`}
+          className={`${props.darkMode}:bg-black ${props.darkMode}:text-white font-header pt-0 md:pb-10 md:pt-20 lg:pt-12 xl:pt-16 text-3xl md:text-4xl md:w-3/5 lg:w-3/5 xl:w-4/5 md:float-left lg:float-left text-center md:text-left`}
         >
           <Typist
             cursor={{
@@ -27,9 +27,9 @@ export default function Introduction(props) {
             London.
           </Typist>
         </div>
-        <div className='md:w-2/5 xl:w-2/5 pl-20 lg:pl-16 xl:pl-24 2xl:pl-44 lg:pt-8 pb-10 lg:pb-16 xl:pt-6 md:float-left lg:float-left md:py-12 xl:pt-0'>
+        <div className='md:w-2/5 xl:w-2/5 lg:pt-8 pb-10 lg:pb-16 xl:pt-6 md:float-left lg:float-left md:py-12 xl:pt-0'>
           <img
-            className='w-48 rounded-full'
+            className='w-48 rounded-full m-auto'
             src={ProfilePic}
             alt='Kiran Rambha'
           />
